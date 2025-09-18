094e43533a77a7e3a24bff74729c749ef0788676
davidevaliante <dav.valiante@gmail.com>
2025-09-18 10:28:04 +0200

    terminal can now be opened even inside oil.nvim

--- lua/custom/plugins/oil-settings.lua
+++ lua/custom/plugins/oil-settings.lua
@@ -4,8 +4,16 @@ return {
   'stevearc/oil.nvim',
   config = function()
     local safe_setup = require('custom.safe_setup')
+
+    -- Get oil keymaps and ensure C-p opens terminal
+    local oil_keymaps = vim.tbl_extend("force", require('custom.keymaps').oil, {
+      ["<C-p>"] = function()
+        vim.cmd('ToggleTerm direction=float')
+      end,
+    })
+
     safe_setup.setup('oil', {
-      keymaps = require('custom.keymaps').oil,
+      keymaps = oil_keymaps,
       columns = {
         "icon",
         "mtime",
